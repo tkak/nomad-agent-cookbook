263ade48776ce4669761479a40247242ef795de6
Takaaki Furukawa <takaaki.frkw@gmail.com>
2017-12-07 08:53:52 +0900

    Fix systemd_service error

--- resources/service.rb
+++ resources/service.rb
@@ -2,18 +2,112 @@ resource_name :nomad_service
 
 property :config_file, String, default: lazy { node['nomad']['config']['path'] }
 property :config_dir, String, default: lazy { node['nomad']['service']['config_dir'] }
+property :data_dir, String, default: lazy { node['nomad']['config']['data_dir'] }
 property :user, String, default: lazy { node['nomad']['service_user'] }
 property :group, String, default: lazy { node['nomad']['service_group'] }
 property :program, String, default: '/usr/local/bin/nomad'
 
-%i(start reload stop enable disable).each do |act|
-  __send__(:action, act, &proc do
-    rs = @new_resource
-    poise_service 'nomad' do
-      template 'systemd.service'
-      command "#{rs.program} agent -config-file=#{rs.config_file} -config-dir=#{rs.config_dir}"
-      action act
-    end
-  end)
+action :start do
+  create_init
+
+  service 'nomad' do
+    supports restart: true, reload: true, status: true
+    provider Chef::Provider::Service::Init::Systemd
+    action [:enable, :start]
+  end
 end
 
+action :stop do
+  service 'nomad' do
+    supports restart: true, reload: true, status: true
+    provider Chef::Provider::Service::Init::Systemd
+    action :stop
+  end
+end
+
+action :restart do
+  service 'nomad' do
+    supports restart: true, reload: true, status: true
+    provider Chef::Provider::Service::Init::Systemd
+    action :restart
+  end
+end
+
+action :reload do
+  service 'nomad' do
+    supports restart: true, reload: true, status: true
+    provider Chef::Provider::Service::Init::Systemd
+    action :reload
+  end
+end
+
+action :enable do
+  create_init
+
+  service 'nomad' do
+    supports restart: true, reload: true, status: true
+    provider Chef::Provider::Service::Init::Systemd
+    action :enable
+  end
+end
+
+action :disable do
+  service 'nomad' do
+    supports restart: true, reload: true, status: true
+    provider Chef::Provider::Service::Init::Systemd
+    action :disable
+  end
+end
+
+action_class do
+  def create_init
+    directory '/etc/systemd/system' do
+      owner 'root'
+      group 'root'
+      mode '0755'
+      recursive true
+      action :create
+    end
+
+    directory new_resource.data_dir do
+      owner new_resource.user
+      group new_resource.group
+      mode '0755'
+      recursive true
+      action :create
+    end
+
+    systemd_unit 'nomad.service' do
+      content service_unit_content
+      action :create
+      notifies :restart, 'service[nomad]', :delayed
+    end
+
+    service 'nomad' do
+      supports restart: true, reload: true, status: true
+      provider Chef::Provider::Service::Init::Systemd
+      action :nothing
+    end
+  end
+
+  def service_unit_content
+    {
+      'Unit' => {
+        'Description' => 'Nomad agent daemon',
+        'Wants' => 'network.target',
+        'After' => 'network.target',
+      },
+      'Service' => {
+        'User' => new_resource.user,
+        'Group' => new_resource.group,
+        'ExecStart' => "#{new_resource.program} agent -config=#{new_resource.config_file} -config=#{new_resource.config_dir}",
+        'ExecReload' => '/bin/kill -HUP $MAINPID',
+        'WorkingDirectory' => new_resource.data_dir,
+        'LimitNOFILE' => 65536,
+      },
+      'Install' => {
+        'WantedBy' => 'multi-user.target',
+      },
+    }
+  end
+end
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -1,6 +1,6 @@
 # # encoding: utf-8
 
-# Inspec test for recipe nomad::default
+# Inspec test for recipe nomad-agent::default
 
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
@@ -12,7 +12,14 @@ unless os.windows?
   end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe systemd_service('consul') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe systemd_service('nomad') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
 end
